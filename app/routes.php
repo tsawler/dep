f91cf2fda2be46c465086f90a1eb973c5b35a505
Trevor Sawler <trevor.sawler@me.com>
2014-01-27 13:10:16 -0400

    Removed commented out routes

--- app/routes.php
+++ app/routes.php
@@ -8,21 +8,11 @@ Route::any('/', function()
 
 Route::controller('/users', 'UserController');
 
+// Routes for Page display and edit
 Route::get('page/{pageid}/{pagename?}', function($pageid,$pagename)
 {
     return View::make('defaultpage');
 })->where('pageid', '[0-9]+');
-
-/*Route::post('/savepage', array('before' => 'auth', function()
-{
-	// make sure we are admin
-	if (Auth::user()->access_level == 3){
-		$page = Page::find(Input::get('page_id'));
-		$page->page_content = Input::get('thedata');
-		$page->save();
-		return "Page updated successfully";
-	}
-}));*/
 
 Route::post('/saveeditedpage', array('before' => 'auth', function()
 {
@@ -36,16 +26,6 @@ Route::post('/saveeditedpage', array('before' => 'auth', function()
 	}
 }));
 
-/*Route::post('/savepagetitle', array('before' => 'auth', function()
-{
-	// make sure we are admin
-	if (Auth::user()->access_level == 3){
-		$page = Page::find(Input::get('page_id'));
-		$page->page_title = Input::get('thetitledata');
-		$page->save();
-		return "Page title updated successfully";
-	}
-}));*/
 
 Route::get('/verifyaccount','UsersPendingController@validateUser');
 
@@ -55,7 +35,7 @@ Route::get('/authors', 'AuthorController@listAuthors');
 
 Route::controller('/submit', 'SubmitController');
 
-// Blog routes
+// Routes for Blog display and edit (other routes in package)
 Route::post('/saveeditedpost', array('before' => 'auth', function()
 {
 	// make sure we are admin

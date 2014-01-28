c25772a03a1eacb8bc6740be4624199dabc5674e
Trevor Sawler <trevor.sawler@me.com>
2014-01-28 09:14:33 -0400

    Added slug column to pages table

---
+++ app/database/migrations/2014_01_28_123433_add_slug_to_pages.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugToPages extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('pages', function($table)
+		{
+		    $table->string('slug');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

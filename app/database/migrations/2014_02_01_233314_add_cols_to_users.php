77dd1b375d49fde6f380294c005a905c212510df
Trevor Sawler <trevor.sawler@me.com>
2014-02-01 19:36:42 -0400

    add two factor auth columns

---
+++ app/database/migrations/2014_02_01_233314_add_cols_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddColsToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+			{
+				$table->integer('use_tfa');
+				$table->string('tfa_secret');
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

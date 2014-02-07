f6b2675f8c00f146b36344358fb0dc147c78931b
Trevor Sawler <trevor.sawler@me.com>
2014-02-07 11:08:54 -0400

    Working on managing users

---
+++ app/views/users/dashboard/adminusers.blade.php
@@ -0,0 +1,46 @@
+@extends('layout')
+
+@section('browser-title')
+Password Change: The Dog-Eared Press
+@stop
+
+@section('content')
+<div class="container clearfix" id="main-content"> 
+	<div class="row-fluid sidebar-right">
+
+		<div class="span9 primary-column"> 
+			<h3 class="short_headline" style="text-transform: none;"><span>Admin Users</span></h3>
+			
+			
+			<table class="responsive table table-striped table-bordered">
+					<thead>
+						<tr>
+							<th> Last name </th>
+							<th> First Name </th>
+							<th> Email </th>
+							<th> Status </th>
+						</tr>
+					</thead>
+					
+					<tbody>
+					@foreach ($adminusers as $user)
+						<tr>
+							<td>{{ $user->last_name }}</td>
+							<td>{{ $user->first_name }}</td>
+							<td>{{ $user->email }}</td>
+							@if($user->user_active == 1)
+							<td><span style="color: green;">Active</span></td>
+							@else
+							<td><span style="color: red;">Inactive</span></td>
+							@endif
+						</tr>
+					@endforeach
+					</tbody>
+			</table>
+		    
+		</div> <!-- /span9 primary column -->
+	
+		@include('users/dashboard/partials/sidemenu')
+	</div>
+</div>
+@stop

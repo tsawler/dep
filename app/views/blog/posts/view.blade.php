d0cff563b5245e82a19f1127330a299af6f69f79
Trevor Sawler <trevor.sawler@me.com>
2014-02-07 22:33:05 -0400

    Completed site search

--- app/views/blog/posts/view.blade.php
+++ app/views/blog/posts/view.blade.php
@@ -33,9 +33,9 @@
 
 			<aside class="widget">
 				<h5 class="short_headline"><span>Search Blog Posts</span></h5>
-				<form class="searchform" method="get">
-					<input type="search" class="span12" value="type and hit enter" onFocus="this.value=''" onBlur="this.value='type and hit enter'" />
-				</form>
+				{{ Form::open(array('url' => '/searchblog', 'class' => 'searchform', 'method' => 'post')) }}
+					{{ Form::text('searchterm', null, array('id' => 'search-box', 'placeholder' => 'type and hit enter')); }}
+				{{ Form::close() }}
 			</aside>
 			@include('blog.partials.archives')
 			

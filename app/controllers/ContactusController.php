57c568f3991c645eae9cd859db6f671a1670b45e
Trevor Sawler <trevor.sawler@me.com>
2014-02-25 13:58:47 -0400

    Completed form handler

--- app/controllers/ContactusController.php
+++ app/controllers/ContactusController.php
@@ -19,7 +19,28 @@ class ContactusController extends BaseController {
 	public function postContactus(){
 		$phrase = Session::get('captcha');
 		if(strtoupper($phrase) == strtoupper(Input::get('captcha'))) {
-		    return "Good! tested " . $phrase . " against " . Input::get('captcha');
+		    
+		    
+		    // build email
+			$user = array(
+				'email'=>Input::get('email'),
+				'name'=>Input::get('name')
+			);
+
+			// the data that will be passed into the mail view blade template
+			$data = array(
+				'users_name'  => $user['name'],
+				'the_message'=>Input::get('message'),
+				'email'=>Input::get('email')
+			);
+			
+			// use Mail::send function to send email passing the data and using the $user variable in the closure
+			Mail::send('emails.contact_email', $data, function($message) use ($user) {
+					$message->from('donotreply@dogearedpress.ca', 'Do not reply');
+					$message->to('query@dogearedpress.ca', 'dogearedpress.ca')->subject('Contact form from dogearedpress.ca');
+			});
+			
+			return Redirect::to('/Thanks');
 		}
 		else {
 		   return Redirect::to('/contactus')

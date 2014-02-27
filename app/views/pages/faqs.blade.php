ab39dc17e0cf6bf0593ca9d98508d932e4294bb6
Trevor Sawler <trevor.sawler@me.com>
2014-02-27 19:32:59 -0400

    Initial entry

---
+++ app/views/pages/faqs.blade.php
@@ -0,0 +1,95 @@
+@extends('layout')
+
+@section('browser-title')
+Frequently Asked Questions: The Dog-Eared Press
+@stop
+
+@section('meta')
+<meta name="description" content="FAQs for the Dog-Eared Press" />
+@stop
+
+
+@section('content')
+
+<div class="container">
+	<h3 class="short_headline" style="text-transform: none;"><span>Frequently Asked Questions</span></h3>
+    
+<ul class="s-accordion faq fancy">
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">Why are you starting a new publishing company? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">Is this a vanity press, or a self-publishing company? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">How much is this going to cost me? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong></p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">Well, eBooks are great, but what about print? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">What about DRM, or Digital Rights Management? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">So what makes this company different? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					<li class="s-wrap">
+						<h4 class="trigger"><a href="#">I've already published with someone else, but I like your model. Can I submit my manuscript? <i class="icon-plus-sign"></i></a></h4>
+						<div class="s-content">
+							<p><strong>Answer:</strong> </p>
+						</div>
+						<!--close s-content--> 
+					</li>
+					<!--close s-wrap-->
+					
+					
+					
+				</ul>
+				<!--close s-accordion.faq --> 	
+
+</div>
+@stop
+
+@section('bottom-js')
+<script>
+    $(document).ready(function() {
+
+    });
+</script>
+@stop

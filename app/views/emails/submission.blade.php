bed4da2c3dc91b94590b5347bb8af7237ab92581
Trevor Sawler <trevor.sawler@me.com>
2014-02-28 13:07:35 -0400

    Corrected to include manuscript details

--- app/views/emails/submission.blade.php
+++ app/views/emails/submission.blade.php
@@ -5,12 +5,6 @@ The Dog-Eared Press
 @stop
 
 
-
-@section('email-content')
-A manuscript has been submitted.
-@stop
-
-
 @section('email-content')
 A manuscript has been submitted.<br />
 User name: {{ $users_name }}:<br /><br />

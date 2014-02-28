3f21174db2a838bce4e59fe8b12a6d1d6c7b508e
Trevor Sawler <trevor.sawler@me.com>
2014-02-28 15:41:54 -0400

    Added processing dialog

--- app/views/submit/index.blade.php
+++ app/views/submit/index.blade.php
@@ -69,7 +69,7 @@ Submit a Manuscript: The Dog-Eared Press
 		<div class="control-group">
 		<div class="controls">
 		<hr>
-	    {{ Form::submit('Submit Manuscript', array('class' => 'btn btn-primary')); }}
+	    {{ Form::submit('Submit Manuscript', array('class' => 'btn btn-primary', 'onclick' => 'myApp.showPleaseWait()')); }}
 	    </div>
 		</div>
 	    
@@ -97,5 +97,18 @@ $(document).ready(function () {
 	    }
 	});
 });
+var myApp;
+myApp = myApp || (function () {
+    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h3>Processing...</h3></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+    return {
+        showPleaseWait: function() {
+            pleaseWaitDiv.modal();
+        },
+        hidePleaseWait: function () {
+            pleaseWaitDiv.modal('hide');
+        },
+
+    };
+})();
 </script>
 @stop

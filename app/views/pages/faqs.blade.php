86459fe797591252211db9012f01c55e52e1d47b
Trevor Sawler <trevor.sawler@me.com>
2014-02-27 20:26:45 -0400

    FAQs

--- app/views/pages/faqs.blade.php
+++ app/views/pages/faqs.blade.php
@@ -18,7 +18,14 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">Why are you starting a new publishing company? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong> </p>
+							<p><strong>Answer:</strong> 
+							We believe that there are a lot of great authors out there who just can't get the attention of an agent
+							or of a traditional publishing house, and we'd like to help them get in front of the reading public. 
+							Many aspiring authors are taking the self-publishing route, and while this works for a very, very small number
+							of writers, most put their work out there and get lost in the crowd. In addition, a lot of self-published
+							authors are desperately in need of an editor&mdash;their work might have great potential, but stylistic, 
+							grammatical, and typographical tend to get in the way.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>
@@ -27,7 +34,13 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">Is this a vanity press, or a self-publishing company? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong> </p>
+							<p><strong>Answer:</strong> 
+								No, this is not a vanity press, and it's not a venue for self-publishing. We will submit each and 
+								every manuscript given to us to a strict referring process. Every document we receive to us will 
+								be carefully reviewed by at least three reviewers before we decide if we want to enter into
+								an arrangement with the author. We are only interested in high quality stories, and our standards
+								are correspondingly high.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>
@@ -36,7 +49,9 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">How much is this going to cost me? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong></p>
+							<p><strong>Answer: </strong>
+								Not one red cent. Ever.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>
@@ -45,7 +60,10 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">How much will I make if I publish with you? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong></p>
+							<p><strong>Answer:</strong>
+								The depends entirely on how many copies are sold, but we can tell you that our royalty arrangement is
+								set at 75% for the writer, and 25% for us.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>
@@ -54,7 +72,12 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">Well, eBooks are great, but what about print? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong> </p>
+							<p><strong>Answer:</strong> 
+								Yeah, we've thought about that a lot, and we intend to pursue print publications after we've been up
+								and running for awhile. There are a lot of ways of going about it: print on demand, printing small runs 
+								until we get a sense of market penetration, and so forth. This is on our radar, but not for the first year.
+								For our most successful titles, we fully intend to go down this road, but not for a year or so.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>
@@ -63,7 +86,14 @@ Frequently Asked Questions: The Dog-Eared Press
 					<li class="s-wrap">
 						<h4 class="trigger"><a href="#">What about DRM, or Digital Rights Management? <i class="icon-plus-sign"></i></a></h4>
 						<div class="s-content">
-							<p><strong>Answer:</strong> </p>
+							<p><strong>Answer:</strong> 
+								Digital Rights Management, or DRM, is a complete and utter waste of time. First, your average twelve year
+								old can circumvent it in after spending roughly ten minutes searching for the tools to do so. Second, having
+								a book that you have bought tied to a specific publisher or a specific device would be a lot like going into your
+								favourite bookstore and being told that you are only allowed to read the book you just bought while you 
+								are physically in that store. It makes no sense whatsoever. If someone intends to pirate a book, they are
+								never going to pay for it in the first place. We'll have no part of any digital rights management system.
+							</p>
 						</div>
 						<!--close s-content--> 
 					</li>

ea3411dd86d49565c9325e94317b02dc7c958299
Trevor Sawler <trevor.sawler@me.com>
2014-03-01 08:36:58 -0400

    session changes for subdomain (image upload)

--- app/events.php
+++ app/events.php
@@ -1,6 +1,8 @@
 <?php
 Event::listen('auth.login', function($user)
 {
+	ini_set('session.cookie_domain', '.dogearedpress.ca' );
+	ini_set("session.cookie_lifetime","86400"); //a day
 	session_start();
     $_SESSION['KCFINDER']['disabled'] = false;
 });

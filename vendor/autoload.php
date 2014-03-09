95a295c3b5c8b7360da4cab3f1dedb54a56d29ef
Trevor Sawler <trevor.sawler@me.com>
2014-03-09 10:26:08 -0300

    composer-update --prefer-dist

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9e45612c35730bbb4cb6bbbb792c34d5::getLoader();
+return ComposerAutoloaderInit8d4e25307a95bb582ddbfc4847f0ebae::getLoader();
